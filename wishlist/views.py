7c225a1bcefde0f62c3206ff3416d40fc5a3038f
ghaydarafa <ghayda.rafa@gmail.com>
2022-09-29 15:46:02 +0700

    Lab 4 Finished

--- wishlist/views.py
+++ wishlist/views.py
@@ -18,7 +18,7 @@ def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
         'list_barang': data_barang_wishlist,
-        'nama': 'Ghayda Rafa Hernawan',
+        'nama': 'Rafa',
         'last_login': request.COOKIES['last_login'],
     }
     return render(request, "wishlist.html", context)
